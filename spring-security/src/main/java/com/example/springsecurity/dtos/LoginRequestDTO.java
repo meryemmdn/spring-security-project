edda8a9dad655d019d8d5faa071aaec8edc9758e
meryemmdn <127888016+meryemmdn@users.noreply.github.com>
2024-01-20 22:27:01 +0100

    Add files via upload

---
+++ spring-security/src/main/java/com/example/springsecurity/dtos/LoginRequestDTO.java
@@ -0,0 +1,15 @@
+package com.example.springsecurity.dtos;
+
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LoginRequestDTO {
+    private String username;
+    private String password;
+}
---
+++ spring-security/src/main/java/com/example/springsecurity/securityconfig/WebSecurityConfiguration.java
@@ -0,0 +1,54 @@
+package com.example.springsecurity.securityconfig;
+
+
+import com.example.springsecurity.filters.JwtTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
+    BCryptPasswordEncoder passwordEncoder;
+    @Autowired
+    UserDetailsService userDetailsService;
+    @Autowired
+    JwtTokenFilter jwtTokenFilter;
+    @Bean
+    public AuthenticationManager getAuthenticationManager() throws Exception {
+        return super.authenticationManagerBean();
+    }
+    @Autowired
+    public void configurePasswordEncoder(AuthenticationManagerBuilder builder) throws Exception {
+// adding custom UserDetailsService and encryption bean to Authentication Manager
+        builder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+
+}
